0d7760dbf8e1e70e6c5f7298abfb1b5836cbc8f3
bar17-meet <bar17@meet.mit.edu>
2016-11-27 19:57:41 +0200

    Update answers.js

--- answers.js
+++ answers.js
@@ -1,6 +1,32 @@
 ///PLEASE ADD YOUR SOLUTION CODE TO PART 1 HERE
 
 ///1. VARIABLES
+var myString = "Hello";
+var myInteger = 1;
+var myDictionary = {name: 'Lorenzo'};
+var temperature = 98.6;
+temperature = temperature + 5;
+temperature = temperature * 3;
+  temperature = temperature - 20;
+alert(temperature);
+
+var myArray= [1,2,3];
+var length= myArray.length; 
+myArray.push(0);
+myArray.pop();
+
+Days_of_the_Week[1]= Sunday
+Days_of_the_Week[2]= Monday
+Days_of_the_Week[3]= Tuesday
+Days_of_the_Week[4]= Wednesday
+Days_of_the_Week[5]= Thursday 
+Days_of_the_Week[6]= Friday
+Days_of_the_Week[7]= Saturday 
+///ILIN IS WITH MEEEE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+///4.  LOOPS///PLEASE ADD YOUR SOLUTION CODE TO PART 1 HERE
+
+//
 
 ///2. DATA STRUCTURES
 
